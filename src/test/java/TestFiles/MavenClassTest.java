ed1e4c6fb81b92882e4d56c46dd914679c241639
Pr <preethiranjithpambel@gmail.com>
2026-02-11 20:57:04 +0530

    Commiting the Demo Project

---
+++ src/test/java/TestFiles/MavenClassTest.java
@@ -0,0 +1,11 @@
+package TestFiles;
+import org.testng.annotations.Test;
+
+public class MavenClassTest {
+
+	@Test
+	public void MynameTest()
+	{
+		System.out.println("testing the maven clean functionality");
+		}
+}
