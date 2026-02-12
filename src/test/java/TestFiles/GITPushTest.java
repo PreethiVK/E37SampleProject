0f5822625fb2e189f4cb180a6a275d25cab807de
Admin <Admin@192.168.1.10>
2026-02-12 20:13:11 +0530

    Commng new Git  clas

---
+++ src/test/java/TestFiles/GITPushTest.java
@@ -0,0 +1,13 @@
+package TestFiles;
+
+import org.testng.annotations.Test;
+
+public class GITPushTest {
+	
+	@Test
+	public void MynameTest()
+	{
+		System.out.println("pushing  the maven clean functionality");
+		}
+
+}
